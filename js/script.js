9ba0db9f7b64642382b49e58371b3d9571458cd0
artur wasek <vaseque@yahoo.co.uk>
2019-11-12 19:30:28 +0100

    Switch off default active state of links & articles

--- js/script.js
+++ js/script.js
@@ -3,19 +3,39 @@
 const titleClickHandler = function(event){
   console.log('Link was clicked!');
   console.log(event);
-
+}
   /* remove class 'active' from all article links  */
+
+const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
 
   /* add class 'active' to the clicked link */
 
+const activeArticles = document.querySelectorAll('.post.active');
+
+for(let activeArticle of activeArticles){
+  activeArticle.classList.remove('active');
+}
+
   /* remove class 'active' from all articles */
+
+
 
   /* get 'href' attribute from the clicked link */
 
+
+
   /* find the correct article using the selector (value of 'href' attribute) */
 
+
+
   /* add class 'active' to the correct article */
-}
+
+
+
 
 const links = document.querySelectorAll('.titles a');
 
